2195bd76869f284d868ef7358db3ac0b88e9b99b
Shaheedz190 <Shaheedz@nycstudents.net>
2026-05-19 11:57:39 -0400

    Update PartB.java

--- question-1/PartB.java
+++ question-1/PartB.java
@@ -1,8 +1,8 @@
 public String getShortenedName(){
   String str= username;
-  while(str.indexOf(-)>-1){
-    int x=str.indexOf(-);
-    str=str.substring(0,x-1)+str.substring(x);
+  while(str.indexOf("-")>-1){
+    int x=str.indexOf("-");
+    str=str.substring(0,x-1)+str.substring(x+1);
   }
   return str;
 }
