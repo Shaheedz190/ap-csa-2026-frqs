77d85ee80082af591f413d3c8ff8d159687a6c17
Shaheedz190 <Shaheedz@nycstudents.net>
2026-05-20 11:22:16 -0400

    Update bottle.java

--- question-2/bottle.java
+++ question-2/bottle.java
@@ -1,1 +1,15 @@
-
+public class Bottle{
+  private double water;
+  private double left;
+  public Bottle(double w){
+    water=w;
+    left=w;
+  }
+  public void updateAmound(double c){
+    left-=c;
+    if (left<water/4){
+      left=water;
+    }
+    return left;
+  }
+}
