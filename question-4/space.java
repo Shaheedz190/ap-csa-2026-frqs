f33de5c3308774226690f4747e1be56b3e31770d
Shaheedz190 <Shaheedz@nycstudents.net>
2026-05-20 11:42:38 -0400

    Update space.java

--- question-4/space.java
+++ question-4/space.java
@@ -1,1 +1,15 @@
-
+public int getPointsForRow(int targetRow){
+  int total=0;
+  boolean dou=true;
+  for(int c=0;c<board[targetRow].length;c++){
+    total+=board[targetRow][c].getPoints();
+    if(board[targetRow][c].getColor()!=board[targetRow][0].getColor()){
+      dou=false;
+    }
+  }
+  if(dou){
+    return total*2;
+  }
+  return total;
+}
+ 
