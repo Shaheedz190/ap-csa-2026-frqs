0caa839484d9614e91cc8e703f86569cd5e0db03
Shaheedz190 <Shaheedz@nycstudents.net>
2026-05-21 11:34:38 -0400

    Update courseRecord.java

--- question-3/courseRecord.java
+++ question-3/courseRecord.java
@@ -1,7 +1,7 @@
 public int moreHistoryThanMath(){
   int count=0;
-  for(int h=0,h<historyList.size();h++){
-    for(int m=0,m<mathList.size();m++){
+  for(int h=0;h<historyList.size();h++){
+    for(int m=0;m<mathList.size();m++){
       if(historyList.get(h).getStudentID()==mathList.get(m).getStudentID()){
         if(historyList.get(h).getAbsences()>mathList.get(m).getAbsences()){
           count++;
